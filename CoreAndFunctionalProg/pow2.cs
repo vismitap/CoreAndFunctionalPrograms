da2a60a73c2596e5f1b764742960282ab122acc2
vismitap <vismita.prabhu@gmail.com>
2022-01-30 23:19:03 +0530

    [Vismita] Add: UC3 PowerOf2 solved

---
+++ CoreAndFunctionalProg/pow2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fellowship_CSharp_Programs
+{
+    public class pow2
+    {
+        public void CalPow2()
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            long pow=1;
+            for (int i=1; i<=num; i++) {
+                pow *= 2; 
+            }
+
+            Console.WriteLine($"2^{num} = "+pow);
+            
+        }
+    }
+}
