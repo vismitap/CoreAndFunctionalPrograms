29c4979b130902d6fc055f96f74a533e9842a44a
vismitap <vismita.prabhu@gmail.com>
2022-01-30 23:10:25 +0530

    [Vismita] Add: UC2 LeapYear solved

---
+++ CoreAndFunctionalProg/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fellowship_CSharp_Programs
+{
+	public class LeapYear
+	{
+		public void IsLeapYear()
+		{
+			Console.WriteLine("Enter any year: ");
+			int year = Convert.ToInt32(Console.ReadLine());
+			if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+			{
+				Console.WriteLine("The " + year + " is a leap year");
+			}
+			else
+			{
+				Console.WriteLine("The " + year + " is NOT a leap year");
+			}
+
+		}
+	}
+}
