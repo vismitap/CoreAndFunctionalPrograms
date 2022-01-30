41b1c5e09dd48c4ff52aa72720e84e39869e44aa
vismitap <vismita.prabhu@gmail.com>
2022-01-30 23:08:05 +0530

    [Vismita] Add: UC1 solved

---
+++ CoreAndFunctionalProg/CoinFlip.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fellowship_CSharp_Programs
+{
+	public class CoinFlip
+	{
+		public void CalCoinFlipPerecnt()
+		{
+			Console.WriteLine("Enter your number of times to flip coin: ");
+			int num = Convert.ToInt32(Console.ReadLine());
+			var random = new Random();
+			int noOfHeads = 0;
+			int noOfTails = 0;
+			int tot = num;
+
+			while (num > 0)
+			{
+				double test = 0;
+				test = random.NextDouble();
+				Console.WriteLine(test);
+				if (test < 0.5)
+				{
+					noOfTails++;
+				}
+				else
+				{
+					noOfHeads++;
+				}
+
+				num--;
+			}
+			double percentHead = ((double)noOfHeads / tot) * 100;
+			double percentTail = 100 - percentHead;
+			Console.WriteLine("Percentage of Head is : " + percentHead + "\nPercentage of Tail is : " + percentTail);
+
+
+
+		}
+	}
+}
